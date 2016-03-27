774781e2995ff72a41972a6ccf8159069f1319ed
kothapardhasaradhi <kothapardhasaradhi@missionrnd.com>
2016-03-27 17:35:15 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -1,3 +1,99 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 
 Problem : Return the Count of how many times the word occurred in given string in two ways
@@ -12,11 +108,36 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 
 #include <stdlib.h>
 
-int count_word_in_str_way_1(char *str, char *word){
+int count_word_in_str_way_1(char *str, char *word)
+{	
+	if (str && word)
+	{
+		int i, j,k, count = 0;
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			for (j = 0,k=i; str[k] != '\0'&&word[j] != '\0'; j++)
+			{
+				if (str[k] == word[j])
+				{
+					k++;
+				}
+				else
+					break;
+			}
+			if (word[j] == '\0')
+			{
+				count++;
+				//i--;
+			}
+		}
+		return count;
+	}
 	return 0;
 }
 
-int count_word_int_str_way_2_recursion(char *str, char *word){
+int count_word_int_str_way_2_recursion(char *str, char *word)
+{
+	
 	return 0;
 }
 
